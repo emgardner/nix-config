ebcf93e5bc88062e9b4cf4d0b2b2fb0cd95f84ea
emgardner <gardner.ethan10@gmail.com>
2026-03-16 22:01:39 -0400

    fix tmux

--- home/ethang/modules/tmux.nix
+++ home/ethang/modules/tmux.nix
@@ -1,14 +1,16 @@
-programs.tmux = {
-  enable = true;
-  extraConfig = '' 
-  # New vertical split (prefix + %) in the current directory
-  bind % split-window -h -c "#{pane_current_path}"
-  
-  # New horizontal split (prefix + ") in the current directory
-  bind '"' split-window -v -c "#{pane_current_path}"
-  
-  # New window (prefix + c) in the current directory
-  bind c new-window -c "#{pane_current_path}"
+{
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      # New vertical split (prefix + %) in the current directory
+      bind % split-window -h -c "#{pane_current_path}"
 
-  '';
+      # New horizontal split (prefix + ") in the current directory
+      bind '"' split-window -v -c "#{pane_current_path}"
+
+      # New window (prefix + c) in the current directory
+      bind c new-window -c "#{pane_current_path}"
+
+    '';
+  };
 }
