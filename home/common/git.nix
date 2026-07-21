037bdc9a21ba7a049d06a0104f740a3a2b5fced1
emgardner <gardner.ethan10@gmail.com>
2026-07-21 14:09:42 -0400

    update

--- home/common/git.nix
+++ home/common/git.nix
@@ -1,9 +1,11 @@
 {
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "emgardner";
-      email = "gardner.ethan10@gmail.com";
+    settings = {
+      user = {
+        name = "emgardner";
+        email = "gardner.ethan10@gmail.com";
+      };
       init.defaultBranch = "main";
       pull.rebase = false;
       credential.helper = "manager";
