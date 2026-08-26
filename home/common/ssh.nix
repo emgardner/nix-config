cc9e61066b8c32d7d3f59b60d9ab2217528d5d62
emgardner <gardner.ethan10@gmail.com>
2026-08-25 20:56:13 -0400

    add ssh aliases to hosts

--- home/common/ssh.nix
+++ home/common/ssh.nix
@@ -1,6 +1,25 @@
 {
   programs.ssh = {
     enable = true;
-    enableDefaultConfig = false;
+    # enableDefaultConfig = false;
+    extraConfig = ''
+      Host ethan-ser9
+          HostName ethan-ser9
+          User ethan
+          Port 22
+          IdentityFile ~/.ssh/id_ed25519
+
+      Host ethan-gti15
+          HostName ethan-gti15
+          User ethang
+          Port 22
+          IdentityFile ~/.ssh/id_ed25519
+
+      Host ethan-nixos
+          HostName nixos
+          User ethang
+          Port 22
+          IdentityFile ~/.ssh/id_ed25519
+    '';
   };
 }
